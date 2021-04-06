7715b89fc9c09c6d390c0131da3cebf05cdec077
FabrizioRozzi <rozzi458@gmail.com>
2021-04-06 13:44:20 +0200

    finito

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var nomeUtente = prompt("Inserisci il tuo nome...");
+var cognomeUtente = prompt("Inserisci il tuo cognome...");
+var coloreUtente = prompt("Inserisci il tuo colore preferito...");
+document.getElementById('psw_utente').innerHTML=("La tua password é: " +nomeUtente+cognomeUtente+coloreUtente+21)
